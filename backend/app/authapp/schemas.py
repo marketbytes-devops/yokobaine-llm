f748b8d29cd38b33a61adcd5eb9447ff7d00afb0
marketbytes-devops <marketbytesdevops@gmail.com>
2026-04-13 10:34:22 +0530

    updatess

--- backend/app/authapp/schemas.py
+++ backend/app/authapp/schemas.py
@@ -18,6 +18,7 @@ class UserBase(BaseModel):
     username: str
     first_name: Optional[str] = None
     last_name: Optional[str] = None
+    phone_number: Optional[str] = None
 
 class UserCreate(UserBase):
     password: str
